16f823dc3604266befe88cbf2c320ed4ffb6a676
ashwin <ashraj98@gmail.com>
2020-11-21 10:01:09 -0500

    Updated text background removal algorithm.

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-from datetime import datetime
-
-import requests
-
-from discovery import DiscoveryClient
-from game import process_game
-
-
-def check_game_processed(game_slug):
-    response = requests.get(f'http://localhost:8000/covers/?game={game_slug}')
-    matches = response.json()
-    return len(matches) > 0
-
+from ocr import extract_text_from_image, remove_text_background
 
 if __name__ == '__main__':
-    # num_rois = label_objects_in_image(path='data/test.jpg')
-    # for i in range(num_rois):
-    #     print(detect_web_objects(path=f'data/images/roi_{i}.png'))
-    # text = cv2.imread('data/text/res.jpg')
-    # roi = cv2.imread('data/images/roi_5.png')
-    # build_cover(text, roi)
-    client = DiscoveryClient()
-    games = client.all_documents()
-    print(f"Started at {datetime.now().strftime('%H:%M:%S')}")
-    for i, game in enumerate(games):
-        if not check_game_processed(game['slug']):
-            process_game(game)
-            print(f"#{i + 1}: Processed {game['name']} at {datetime.now().strftime('%H:%M:%S')}")
-        else:
-            print(f"#{i + 1}: Skipped {game['name']} at {datetime.now().strftime('%H:%M:%S')}")
+    # extract_text_from_image(path='data/cover.jpg')
+    remove_text_background(path='data/text/res.jpg')
     exit(0)
--- ocr.py
+++ ocr.py
@@ -1,3 +1,5 @@
+import sys
+
 import cv2
 from google.cloud import vision
 import io
@@ -29,7 +31,6 @@ def extract_text_from_image(path):
 
     if max_poly is None:
         return False
-    cv2.rectangle(img, max_poly[0], max_poly[2], (36, 255, 12), 2)
     roi = img[max_poly[0][1]:max_poly[2][1], max_poly[0][0]:max_poly[2][0]]
     cv2.imwrite('data/text/res.jpg', roi)
     return True
@@ -39,10 +40,32 @@ def remove_text_background(path):
     image = cv2.imread(path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.medianBlur(gray, 5)
-    edges = cv2.Canny(blurred, 50, 100)
-    morph_kernel_size = 4
-    kernel = np.ones((morph_kernel_size, morph_kernel_size))
-    # do a morphological close
-    res = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
-    cv2.imshow('result', res)
+    sh, sw = gray.shape
+    edge_color =\
+        (np.sum(gray[0:, 0]) + np.sum(gray[0, 0:]) + np.sum(gray[0:, sw-1]) + np.sum(gray[sh-1, 0:])) / ((sh + sw) * 2)
+    print(edge_color)
+    pad_color = 0 if edge_color else 255
+    border_size = 10
+    bordered = cv2.copyMakeBorder(
+        blurred,
+        top=border_size,
+        bottom=border_size,
+        left=border_size,
+        right=border_size,
+        borderType=cv2.BORDER_CONSTANT,
+        value=pad_color
+    )
+    # ret, thresh = cv2.threshold(bordered, 127, 255, cv2.THRESH_BINARY)
+    thresh = cv2.adaptiveThreshold(bordered, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    edges = cv2.Canny(thresh, 50, 100)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    close = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+    h, w = bordered.shape[:2]
+    mask = np.zeros((h + 2, w + 2), np.uint8)
+    # Flood fill from point (0, 0)
+    cv2.floodFill(close, mask, (0, 0), 255)
+    no_border = close[border_size:h-border_size, border_size:w-border_size]
+    rgb = cv2.cvtColor(no_border, cv2.COLOR_GRAY2RGB)
+    text_only = cv2.subtract(image, rgb)
+    cv2.imshow('result', text_only)
     cv2.waitKey()
