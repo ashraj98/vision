8cb2c68658a3c407bc827d0c57175ffab1d33e5f
ashwin <ashraj98@gmail.com>
2020-12-01 13:06:54 -0500

    Updated model params.

--- fonts/model.py
+++ fonts/model.py
@@ -4,30 +4,30 @@ from tensorflow.keras.models import Sequential
 
 model = Sequential()
 
-model.add(Conv2D(filters=64, kernel_size=(5, 5), activation='relu', input_shape=(100, 500, 3)))
+model.add(Conv2D(filters=4, kernel_size=(5, 5), activation='relu', input_shape=(100, 500, 3)))
 model.add(BatchNormalization(axis=3))
-model.add(Conv2D(filters=64, kernel_size=(5, 5), activation='relu'))
+model.add(Conv2D(filters=4, kernel_size=(5, 5), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(BatchNormalization(axis=3))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(filters=128, kernel_size=(5, 5), activation='relu'))
+model.add(Conv2D(filters=8, kernel_size=(5, 5), activation='relu'))
 model.add(BatchNormalization(axis=3))
-model.add(Conv2D(filters=128, kernel_size=(5, 5), activation='relu'))
+model.add(Conv2D(filters=8, kernel_size=(5, 5), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(BatchNormalization(axis=3))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(filters=256, kernel_size=(5, 5), activation='relu'))
+model.add(Conv2D(filters=16, kernel_size=(5, 5), activation='relu'))
 model.add(BatchNormalization(axis=3))
-model.add(Conv2D(filters=256, kernel_size=(5, 5), activation='relu'))
+model.add(Conv2D(filters=16, kernel_size=(5, 5), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(BatchNormalization(axis=3))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
 
-model.add(Dense(256, activation="relu"))  # Fully connected layer
+model.add(Dense(125, activation="relu"))  # Fully connected layer
 model.add(BatchNormalization())
 model.add(Dropout(0.5))
 
